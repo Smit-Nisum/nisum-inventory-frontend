9c782b95f4d67ba7d28eaf26fb0bd2893c51dea4
tommylay1902 <tommylay.c@gmail.com>
2022-09-21 10:53:35 -0700

    added materials, created infinite scroll

--- angular-inventory/src/app/app.module.ts
+++ angular-inventory/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MatTableModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- angular-inventory/src/app/product-list/product-list.component.ts
+++ angular-inventory/src/app/product-list/product-list.component.ts
@@ -9,6 +9,35 @@ import { ProductService } from 'src/shared/services/product.service';
 export class ProductListComponent implements OnInit {
   products: any[] = [];
 
+  // <th scope="col">UPC</th>
+  // <th scope="col" >Product Name</th>
+  // <th scope="col">Category</th>
+  // <th scope="col">Price Per Unit</th>
+  // <th scope="col">Available Stock</th>
+  // <th scope="col">Reserved Stock</th>
+  // <th scope="col">Shipped Stock</th>
+  // <th scope="col">View/Edit</th>
+  // <th scope="col">Delete</th>
+  displayedColumns = [
+    'upc',
+    'prodName',
+    'category',
+    'pricePerUnit',
+    'availableStock',
+    'reservedStock',
+    'shippedStock',
+    'View/Edit',
+    'Delete',
+  ];
+  /*
+    Start and end index representing how many table cells are present 
+    in the table  
+  */
+  start: number = 0;
+  limit: number = 15;
+  end: number = this.limit + this.start;
+  selectedRowIndex: any = null;
+
   constructor(private ps: ProductService) {}
 
   ngOnInit(): void {
@@ -16,5 +45,50 @@ export class ProductListComponent implements OnInit {
       console.table(products);
       this.products = products;
     });
+  }
+  onClickSort() {}
+
+  /*
+    Table scroll event handler
+  */
+  onTableScroll(e: any) {
+    console.log(e);
+    const tableViewHeight = e.target.offsetHeight; // viewport
+    const tableScrollHeight = e.target.scrollHeight; // length of all table
+    const scrollLocation = e.target.scrollTop; // how far user scrolled
+
+    // If the user has scrolled within 200px of the bottom, add more data
+    const buffer = 200;
+    const limit = tableScrollHeight - tableViewHeight - buffer;
+    if (scrollLocation > limit) {
+      let data = this.getTableData(this.start, this.end);
+      this.products = this.products.concat(data);
+      this.updateIndex();
+    }
+  }
+
+  /*
+    Filter prodcuts to get products in range
+  */
+  getTableData(start: any, end: any) {
+    return this.products.filter(
+      (value: any, index: any) => index >= start && index < end
+    );
+  }
+
+  /*
+    updates the index position of where you are in the table currently
+  */
+  updateIndex() {
+    this.start = this.end;
+    this.end = this.limit + this.start;
+  }
+
+  /*
+    Will return the dom reference of the current row selected
+    will get called on button click by default
+  */
+  selectedRow(row: any) {
+    console.log('selectedRow', row);
   }
 }
