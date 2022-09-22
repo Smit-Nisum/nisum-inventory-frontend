1bbe049bd927b599d0e8382c170f337214258c91
TinThuZarAye-TTZA-github <tinthuzaraye.github12@gmail.com>
2022-09-22 09:43:11 -0700

    Delete UI - pop up window

--- angular-inventory/src/app/app-routing.module.ts
+++ angular-inventory/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [{ path: 'products-list', component: ProductListComponent }];
+const routes: Routes = [{ path: 'products', component: ProductListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
