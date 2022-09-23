ad8432fab34714ac50f2b789fac5d17cda450cf5
Tam Nguyen <60633642+tamnguy443@users.noreply.github.com>
2022-09-23 13:14:17 -0700

    Added Routing to google SSO
    Added authguard.guard files for authentication. the app now automatically takes the user to login page if not signed in, only after signing in will the user be taken directly to the product view page with a logout button

--- angular-inventory/src/app/app-routing.module.ts
+++ angular-inventory/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthguardGuard } from 'src/shared/services/authguard.guard';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [{ path: 'products', component: ProductListComponent }, 
-                        { path: '/login-page', component: LoginPageComponent }];
-
+const routes: Routes = [{ path: 'products', component: ProductListComponent, canActivate: [AuthguardGuard] },
+                        { path: 'login-page', component: LoginPageComponent },
+                        { path: '', redirectTo: 'login-page', pathMatch: 'full' }];
+//, canActivate: [AuthGuardService]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
