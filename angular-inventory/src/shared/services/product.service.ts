afd62ca4156bff137de7a8c997f9a6345df6565f
tommylay1902 <tommylay.c@gmail.com>
2022-09-27 11:17:06 -0700

    added environment variable instead of hard coded url

--- angular-inventory/src/shared/services/product.service.ts
+++ angular-inventory/src/shared/services/product.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  baseUrl: string = `http://localhost:8080/api/products`;
+  baseUrl: string = environment.baseUrl;
   upcValue: string;
 
   constructor(private http: HttpClient) {}
